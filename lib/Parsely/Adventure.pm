cf0cf9214d83b2eaef7895c785c6352e03543671
Jason A. Crome <jason@crome-plated.com>
2017-09-11 16:22:15 -0500

    WIP: adding locations to an adventure.

--- lib/Parsely/Adventure.pm
+++ lib/Parsely/Adventure.pm
@@ -1,18 +1,27 @@
 package Parsely::Adventure;
 
-use lib '.';
 use Moo;
 use MooX::Types::MooseLike::Base qw/:all/;
 use YAML 'LoadFile';
+
 use v5.24;
 use strictures 2;
 use feature qw(signatures);
 no warnings qw(experimental::signatures);
+
+use lib '.';
 #use Parsely::Base;
+use Parsely::Location;
+use Data::Dumper;
 
 has name => (
     is  => 'rw',
     isa => Str,
+);
+
+has locations => (
+    is  => 'rw',
+    isa => ArrayRef,
 );
 
 # Load a new adventure
@@ -23,7 +32,9 @@ sub load( $self, $adventure ) {
     if( -e $file ) {
         my $config = LoadFile( $file );
         if( $config and $self->validate( $config )) {
+            # Everything is valid at this point, add with impunity
             $self->name( $config->{ name } );
+            $self->_load_locations( $config );
         }
     }
     else {
@@ -37,9 +48,36 @@ sub validate( $self, $config ) {
     return 1;
 }
 
+# TODO: Validate actors
+# TODO: Validate items
 # TODO: Validate Area
 # TODO: Validate exits
-# TODO: Validate items
+# TODO: Validate game over conditions
+
+sub _load_locations( $self, $config ) {
+    die "No adventure configuration!" unless $config;
+
+    for my $location( keys %{ $config->{ locations }}) {
+        my $loc_info = $config->{ locations }->{ $location };
+        my $room     = Parsely::Location->new;
+
+        $room->slug( $location );
+        $room->initial_description( 
+            $loc_info->{ initial_description } // $loc_info->{ description } );
+        $room->description( $loc_info->{ description } );
+        $room->name( $loc_info->{ name } );
+
+        push @{ $room->items  }, $_ foreach @{ $loc_info->{ items  }};
+        push @{ $room->actors }, $_ foreach @{ $loc_info->{ actors }};
+
+        my %ways_out;
+        for my $exit( keys %{ $loc_info->{ exits }} ) {
+            $ways_out{ $exit } = $loc_info->{ exits }{ $exit };
+        }
+        $room->exits( \%ways_out );
+        #die Dumper $room;
+    }
+}
 
 1;
 
---
+++ lib/Parsely/Location.pm
@@ -0,0 +1,72 @@
+package Parsely::Location;
+
+use lib '.';
+use Moo;
+use MooX::Types::MooseLike::Base qw/:all/;
+use YAML 'LoadFile';
+use v5.24;
+use strictures 2;
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
+#use Parsely::Base;
+
+has slug => (
+    is  => 'rw',
+    isa => Str,
+);
+
+has name => (
+    is  => 'rw',
+    isa => Str,
+);
+
+has initial_description => (
+    is  => 'rw',
+    isa => Str,
+);
+
+has description => (
+    is  => 'rw',
+    isa => Str,
+);
+
+has visited => (
+    is      => 'rw',
+    isa     => Bool,
+    default => 0,
+);
+
+has actors => (
+    is  => 'rw',
+    isa => ArrayRef,
+);
+
+has items => (
+    is  => 'rw',
+    isa => ArrayRef,
+    default => sub{[]},
+);
+
+has exits => (
+    is  => 'rw',
+    isa => HashRef,
+);
+
+has looks => (
+    is  => 'rw',
+    isa => HashRef,
+);
+
+has actions => (
+    is  => 'rw',
+    isa => HashRef,
+);
+
+has properties => (
+    is  => 'rw',
+    isa => HashRef,
+);
+
+1;
+
+
